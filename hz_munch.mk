50c395fdb3d5c6f4757c5e35c0434d76c3ed9874
HDzungx <hdzungx@gmail.com>
2023-11-06 19:43:52 +0700

    munch: HZOS

--- hz_munch.mk
+++ hz_munch.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/hz/config/common_full_phone.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := cherish_munch
+PRODUCT_NAME := hz_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 
@@ -23,10 +23,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit some props from Cherish
-CHERISH_BUILD_TYPE := OFFICIAL
+# Inherit some props from hz
+HZ_BUILD_TYPE := OFFICIAL
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=HDzungx
+    ro.hz.maintainer=HDzungx
 
 # Inherit some common device props
 TARGET_SUPPORTS_QUICK_TAP := true
